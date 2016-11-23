5eb8c0f1275b6c2c593203627f1a594e01f0de54
manolosierra99 <manolo.sierra99@gmail.com>
2016-11-23 11:36:54 +0100

    Aplicacion funcion de Fibonacci

--- Fibonacci/Aplicacion_Fibonacci.py
+++ Fibonacci/Aplicacion_Fibonacci.py
@@ -3,3 +3,8 @@ Created on 23 nov. 2016
 
 @author: usuario
 '''
+from Fibonacci_package.Funcion_Fibonacci import Fibonacci
+if __name__ =="__main__":
+    numero=int(input("Escribe un numero sobre el que desea saber la funcion de Fibonacci:"))
+    print(Fibonacci(numero))
+
